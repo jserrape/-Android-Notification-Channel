d6ea32a74da769cee34a7828a34733562cc09ff0
xenahort <juan.carlos.wow.95@gmail.com>
2019-04-30 17:59:52 +0200

    Ya realiza la notificación

--- NotificationStatusBar/app/src/main/java/es/gidm/notificationstatusbar/NotificationStatusBarActivity.java
+++ NotificationStatusBar/app/src/main/java/es/gidm/notificationstatusbar/NotificationStatusBarActivity.java
@@ -1,64 +1,92 @@
 package es.gidm.notificationstatusbar;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioAttributes;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class NotificationStatusBarActivity extends AppCompatActivity {
-  private static final int ID_NOTIFICACION = 2;
+    private static final int ID_NOTIFICACION = 2;
 
-  private int contador = 0;
 
-  private final CharSequence ticker =
-      "Un mensaje de notificación largo para que se divida en varias líneas";
-  private final CharSequence titulo = "Notificación";
-  private final CharSequence contenido = "Has recibido una notificación";
+    private int contador = 1;
 
-  private Intent notificacionIntent;
-  private PendingIntent contenidoIntent;
+    private final String id = "123";
 
-  private Uri sonidoURI = Uri
-      .parse("android.resource://es.gidm.notificationstatusbar/"
-          + R.raw.ding);
-  private long[] patronVibracion = {0, 200, 200, 300};
+    private final String ticker = "Un mensaje de notificación largo para que se divida en varias líneas";
+    private final String titulo = "Notificación";
+    private final String contenido = "Has recibido una notificación";
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
 
-    setContentView(R.layout.activity_main);
+    private Uri sonidoURI = Uri.parse("android.resource://es.gidm.notificationstatusbar/" + R.raw.ding);
+    private long[] patronVibracion = {0, 200, 200, 300};
 
-    notificacionIntent = new Intent(getApplicationContext(),
-        NotificationSubActivity.class);
-    contenidoIntent = PendingIntent.getActivity(getApplicationContext(), 0,
-        notificacionIntent, 0);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    final Button boton = (Button) findViewById(R.id.boton);
-    boton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Notification.Builder notificationBuilder =
-            new Notification.Builder(getApplicationContext())
-                .setTicker(ticker)
-                .setSmallIcon(android.R.drawable.stat_sys_warning)
-                .setAutoCancel(true)
-                .setContentTitle(titulo)
-                .setContentText(contenido + " (" + ++contador + ")")
-                .setContentIntent(contenidoIntent);
-        //.setSound(sonidoURI)
-        //.setVibrate(patronVibracion);
+        setContentView(R.layout.activity_main);
 
-        NotificationManager notificationManager = (NotificationManager)
-            getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(ID_NOTIFICACION, notificationBuilder.build());
-      }
-    });
-  }
+        createNotificationChannel();
+
+        final Button boton = (Button) findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                Notification.Builder notificationBuilder =
+                        new Notification.Builder(getApplicationContext())
+                                .setTicker(ticker)
+                                .setSmallIcon(android.R.drawable.stat_sys_warning)
+                                .setAutoCancel(true)
+                                .setContentTitle(titulo)
+                                .setContentText(contenido + " (" + ++contador + ")")
+                                .setContentIntent(contenidoIntent);
+                //.setSound(sonidoURI)
+                //.setVibrate(patronVibracion);
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.notify(ID_NOTIFICACION, notificationBuilder.build());
+                */
+
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    Notification.Builder nb = new Notification.Builder(getApplicationContext(), id)
+                            .setContentTitle(getString(R.string.notificacion)) //Titulo
+                            .setContentText(getString(R.string.n_notificacion) + " " + contador) //Contenido
+                            .setSmallIcon(R.drawable.ic_warning_black_24dp) //Añado un icono
+                            .setAutoCancel(true); //Eliminar al hacer click
+
+                    NotificationManager mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    mManager.notify(101, nb.build());
+
+                    ++contador;
+                }
+            }
+        });
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            (Toast.makeText(getApplicationContext(), "Creación del canal", Toast.LENGTH_SHORT)).show();
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(id, titulo, importance);
+            channel.setDescription(contenido);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        } else {
+            (Toast.makeText(getApplicationContext(), "Versión de Android inferior a API 26", Toast.LENGTH_SHORT)).show();
+        }
+    }
 }
